6709be93fbceebcfab17a276f7ed05ec3c20b091
Brooks Patton <brooks@brookzerker.com>
2020-06-29 07:32:57 -0600

    added water

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const webSocket = new WebSocket("ws://localhost:8080");
-
 webSocket.onmessage = (event) => console.log(event);
+
+let worldUnitSize = 30;
+
+function setup() {
+  createCanvas(1920, 1080);
+}
+
+function draw() {
+  drawBackground();
+}
+
+function drawBackground() {
+  fill(0, 0, 255);
+  rect(0, height - worldUnitSize, width, worldUnitSize);
+}
