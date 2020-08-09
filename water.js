c8f2be78ce6b6c10f48386356225d4f60b95d8c5
Bare <bareknuckle@nah.com>
2020-08-09 13:23:07 -0400

    make water prettier
    adds a blue shift based on wave height.
    adds a cyan channel visible on tall waves.
    adds some trransparency based on wave height.
    wave segments are closer together for prettiness.

--- water.js
+++ water.js
@@ -1,13 +1,13 @@
 class Water {
   constructor() {
-    this.xspacing = 8; // Distance between each horizontal location
+    this.xspacing = 2; // Distance between each horizontal location
     this.w = width + this.xspacing; // Width of entire wave
     this.theta = 0.0; // Start angle at 0
     this.amplitude = 5; // Height of wave
-    this.period = 250.0; // How many pixels before the wave repeats
+    this.period = 400.0; // How many pixels before the wave repeats
     this.dx = (TWO_PI / this.period) * this.xspacing; // Value for incrementing x
     this.yvalues = new Array(floor(this.w / this.xspacing)); // Using an array to store height values for the wave
-    this.centerLocationY = height - this.amplitude / 2;
+    this.centerLocationY = height + 2 - this.amplitude / 2;
     this.color = color(0, 0, 255, 10);
   }
 
@@ -17,15 +17,16 @@ class Water {
 
   render() {
     noStroke();
-    fill(this.color);
+    // fill(this.color);
     // A simple way to draw the wave with an ellipse at each location
     for (let x = 0; x < this.yvalues.length; x++) {
-      ellipse(
-        x * this.xspacing,
-        this.centerLocationY + this.yvalues[x],
-        24,
-        24
-      );
+      let alpha = (((this.yvalues[x] - -this.amplitude) * (150 - 50)) / (this.amplitude - -this.amplitude)) + 50;
+      let hmod = (((this.yvalues[x] - -this.amplitude) * (0 - 2)) / (this.amplitude - -this.amplitude)) + 2;
+      fill(0, 255, 255, alpha);
+      ellipse(x * this.xspacing, this.centerLocationY + this.yvalues[x] + -hmod, 8, 32);
+      let color = (((this.yvalues[x] - -this.amplitude) * (80 - 255)) / (this.amplitude - -this.amplitude)) + 255;
+      fill(0, 0, color, alpha);
+      ellipse(x * this.xspacing, this.centerLocationY + this.yvalues[x], 16, 32);
     }
   }
 
